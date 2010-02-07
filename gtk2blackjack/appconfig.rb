d2874ea9c43eff8d925f1dfacb74c782a74cbcf4
Carlos Hernandez <cjhr64@linux-hia0.site>
2010-02-07 10:25:05 -0800

    data hierarchy issues

--- gtk2blackjack/appconfig.rb
+++ gtk2blackjack/appconfig.rb
@@ -1,8 +1,10 @@
 module Configuration
   CANVAS_IMAGE_DIRECTORY = UserSpace::DIRECTORY+'/pngs/'
   BACKGROUND_COLOR = COLOR[:green]
-  WIDGET_OPTIONS[:font] = FONT[:small]
-  WIDGET_OPTIONS[:label_fg] = {Gtk::STATE_NORMAL => COLOR[:white]}
-  WIDGET_OPTIONS[:image_bg] = {Gtk::STATE_NORMAL => BACKGROUND_COLOR}
+  OPTIONS	= {
+	:font		=> FONT[:small],
+	:label_fg	=> {Gtk::STATE_NORMAL => COLOR[:white]},
+	:image_bg	=> {Gtk::STATE_NORMAL => BACKGROUND_COLOR},
+  }
   MENU[:fs] = '_Fullscreen' if Gtk2App::HILDON
 end
