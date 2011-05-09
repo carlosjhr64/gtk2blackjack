484698b040b285fadf937f85f1aa8eafd6df2d1c
CarlosJHR64 <carlosjhr64@gmail.com>
2011-05-09 07:51:37 -0700

    using applib 15.3

--- lib/gtk2blackjack/appconfig.rb
+++ lib/gtk2blackjack/appconfig.rb
@@ -1,11 +1,16 @@
 module Gtk2AppLib
 module Configuration
-  BACKGROUND_COLOR = Color[/hooker.*green/i]
+  MENU[:fs] = '_Fullscreen' if HILDON
+end
+end
+
+module Gtk2BlackJack
+module Configuration
+  BACKGROUND_COLOR = Gtk2AppLib::Color[/hooker.*green/i]
   OPTIONS	= {
-	:modify_font	=> FONT[:Small],
-	:modify_fg	=> [Gtk::STATE_NORMAL, Color[/white/i]],
+	:modify_font	=> Gtk2AppLib::Configuration::FONT[:SMALL],
+	:modify_fg	=> [Gtk::STATE_NORMAL, Gtk2AppLib::Color[/white/i]],
 	:modify_bg	=> [Gtk::STATE_NORMAL, BACKGROUND_COLOR],
   }
-  MENU[:fs] = '_Fullscreen' if HILDON
 end
 end
